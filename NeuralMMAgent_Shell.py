f4bdc817211c9d667eef6d0c63e78a80be282c02
Dan Dean <DPD5518@psu.edu>
2023-04-17 20:44:01 -0400

    The feed forward should now work

--- NeuralMMAgent_Shell.py
+++ NeuralMMAgent_Shell.py
@@ -34,26 +34,52 @@ class NeuralMMAgent(object):
             random_seed -- used to seed object random attribute.
                 This ensures that we can reproduce results if wanted
         '''
-        assert num_in_nodes > 0 and num_hid_layers > 0 and num_hid_nodes and\
-            num_out_nodes > 0, "Illegal number of input, hidden, or output layers!"
+        assert num_in_nodes > 0 and num_hid_layers > 0 and num_hid_nodes and num_out_nodes > 0, "Illegal number of input, hidden, or output layers!"
+        self.num_in_nodes = num_in_nodes
+        self.num_hid_nodes = num_hid_nodes
+        self.num_hid_layers = num_hid_layers
+        self.num_out_nodes = num_out_nodes
+        self.learning_rate = learning_rate
+        self.max_epoch = max_epoch
+        self.min_sse = min_sse
+        self.momentum = momentum
+        self.creation_function = creation_function
+        self.activation_function = activation_function
+        self.random_seed = random_seed
 
+        #WE CAN SIMPLY FILL OUT THESE VALUES LATER AS WE CALCULATE THEM, BUT WE INIT HERE
+        self.weights = []
+        self.weight_ds = []
+        self.activations = []
+        self.net = []
+        self.errors = []
+        self.bias = []
+        self.bias_ds = []
+                
+    ##################################################################################################
+    #DONE FUNCTIONS
+    ##################################################################################################
+    @staticmethod
+    def sigmoid_af(summed_input):
+        #Sigmoid function
+        e_to_the = numpy.exp #pythonic
+        denom = 1+e_to_the(-summed_input)
+        return 1/denom 
 
-    def train_net_incremental(self, input_list, output_list, max_num_epoch=100000, \
-                    min_sse=0.001):
-        ''' Trains neural net using incremental learning
-            (update once per input-output pair)
-            Arguments:
-                input_list -- 2D list of inputs
-                output_list -- 2D list of outputs matching inputs
-            Outputs:
-                1d list of errors (total error each epoch) (e.g., [0.1])
-        '''
-                #Some code...#
-            #all_err.append(total_err)
+    @staticmethod
+    def sigmoid_af_deriv(sig_output):
+        #the derivative of the sigmoid function"
+        return sig_output * (1-sig_output)
 
-            #if (total_err < min_sse):
-                #break
-
+    def matrixify_weigths(self,layer):
+        layer_in_question = weights[layer]
+        cols = list()
+        for i in range(len(layer_in_question)//self.num_hid_nodes):
+            cols.append(self._get_incoming_weights(layer+1,i))
+        return cols
+    
+    ##################################################################################################
+    #FEED FORWARD
     def _feed_forward(self, input_list, row):
         '''Used to feedforward input and calculate all activation values
             Arguments:
@@ -62,74 +88,42 @@ class NeuralMMAgent(object):
             Outputs:
                 list of activation values
         '''
-        pass
+        return self.__feedforward(input_list[row])
+    
+    
+    
+    
+    def __feedforward(self,inp):
+        #simply shoe the input layer in there
+        self.activations = [ [float(i) for i in inp] ]
+        # with biases
+        for i in range(len(self.activations[0])): self.activations[0][i] += self.bias[0][i]
+        for i in range(self.num_hid_layers+1):
+            # change the order of the weights so we can take the dot product with the previous layer of activations 
+            self.activations.append(NeuralMMAgent.sigmoid_af(numpy.dot(numpy.matrix(self.matrixify_weigths(i)),self.activations[-1])+self.bias[i+1]).tolist()[0])
+        return self.activations
+    
+    def _get_incoming_weights(self,layer_index,node_index):
+        incoming_weights = list()
+        if not layer_index: return [] #zeroth layer is input should not have incoming weights
+                                      # is it a hidden node?                        or is it an output node?
+        depth = self.num_hid_nodes*(layer_index <= self.num_hid_layers) + self.num_out_nodes*(layer_index > self.num_hid_layers)
+        for i in range(node_index,len(self.weights[layer_index-1]),depth):
+            incoming_weights.append(self.weights[layer_index-1][i])
+        return incoming_weights
 
-    def _calculate_deltas(self, activations, errors, prev_weight_deltas=None):
-        '''Used to calculate all weight deltas for our neural net
-            Parameters:
-                activations -- a 2d list of activation values
-                errors -- a 2d list of errors
-                prev_weight_deltas [OPTIONAL] -- a 2d list of previous weight deltas
-            Output:
-                A tuple made up of 3 items:
-                    A 2d list of little deltas (e.g., [[0, 0], [-0.1, 0.1], [0.1]])
-                    A 2d list of weight deltas (e.g., [[-0.1, 0.1, -0.1, 0.1], [0.1, 0.1]])
-                    A 2d list of bias deltas (e.g., [[0, 0], [-0.1, 0.1], [0]])
-        '''
-
-        #Calculate error gradient for each output node & propgate error
-        #   (calculate weight deltas going backward from output_nodes)
-
-
-
-
-    def _adjust_weights_bias(self, weight_deltas, bias_deltas):
-        '''Used to apply deltas
-        Parameters:
-            weight_deltas -- 2d list of weight deltas
-            bias_deltas -- 2d list of bias deltas
-        Outputs:
-            A tuple w/ the following items (in order):
-            2d list of all weights after updating (e.g. [[-0.071, 0.078, 0.313, 0.323], [-0.34, 0.021]])
-            list of all biases after updating (e.g., [[0, 0], [0, 0], [0]])
-        '''
-
-    #########ACCESSORS
-
-    def get_weights(self):
-        return (self.weights)
-
-    def set_weights(self, weights):
-        self.weights = weights
-
-    def get_bias(self):
-        return (self.bias)
-
-    def set_biases(self, bias):
-        self.bias = bias
-
-    ################
-
-    @staticmethod
-    def sigmoid_af(summed_input):
-        #Sigmoid function
-        pass
-
-    @staticmethod
-    def sigmoid_af_deriv(sig_output):
-        #the derivative of the sigmoid function
-        pass
-
-
-#----#
-#Some quick test code
-"""
-test_agent = NeuralMMAgent(2, 2, 1, 1,random_seed=5, max_epoch=1000000, \
-                            learning_rate=0.2, momentum=0)
-test_in = [[1,0],[0,0],[1,1],[0,1]]
-test_out = [[1],[0],[0],[1]]
-test_agent.set_weights([[-.37,.26,.1,-.24],[-.01,-.05]])
-test_agent.set_biases([[0,0],[0,0],[0,0]])
-all_errors = test_agent.train_net(test_in, test_out, max_sse = test_agent.max_sse, \
-                     max_num_epoch = test_agent.max_epoch)
-"""
+    
+    def _get_outgoing_weights(self,layer_index,node_index):
+        if layer_index == self.num_hid_layers+1: return []
+        assert(len(self.weights[layer_index])>(node_index))
+        
+        outgoing_weights = list()
+        depth = self.num_hid_nodes*(layer_index <= self.num_hid_layers) + self.num_out_nodes*(layer_index > self.num_hid_layers)
+        if layer_index == self.num_hid_layers+1: return []
+        #a straightforward way of getting the # of nodes in the next layer 
+        nodes_in_next_layer = self.num_hid_nodes*(layer_index+1 <= self.num_hid_layers) + self.num_out_nodes*(layer_index+1 > self.num_hid_layers)
+        for i in range(0,nodes_in_next_layer):
+            outgoing_weights.append(self.weights[layer_index][node_index*nodes_in_next_layer +i])
+        return outgoing_weights
+    ##################################################################################################
+    
